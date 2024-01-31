2f888eab58958b50e38be86c87bd9a675c17c92c
vrena4567 <v.rena4567@gmail.com>
2024-01-31 19:44:43 +0100

    role entity added, role repo unit test added

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme.admin/ShopmeBackEndApplication.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme.admin/ShopmeBackEndApplication.java
@@ -2,8 +2,11 @@ package com.shopme.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.shopme.common.entity", "com.shopme.admin.user"})
+//since these classes are in different folders, springboot app could not find them, therefore we use this annotation
 public class ShopmeBackEndApplication {
 
 	public static void main(String[] args) {
